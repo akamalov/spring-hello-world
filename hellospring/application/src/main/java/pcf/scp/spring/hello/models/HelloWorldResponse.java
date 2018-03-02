5a12d8c09d83d384dac9413bf835131535e82738
mblakeashley <m.blakeashley@gmail.com>
2018-03-02 19:19:41 +0000

    initial commit

---
+++ hellospring/application/src/main/java/pcf/scp/spring/hello/models/HelloWorldResponse.java
@@ -0,0 +1,11 @@
+package pcf.scp.spring.hello.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class HelloWorldResponse
+{
+    @JsonProperty("hello")
+    private final String name;
+}
